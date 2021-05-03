f4878dba10a571567c6cf0c505137d7d3b4d6cdb
soumoq <soumojyotiganguly270@gmail.com>
2021-05-03 22:32:49 +0530

    init

---
+++ app/src/main/java/com/arobit/serviceexample/ExampleBroadcastReceiver.java
@@ -0,0 +1,13 @@
+package com.arobit.serviceexample;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+public class ExampleBroadcastReceiver extends BroadcastReceiver {
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+
+    }
+}
